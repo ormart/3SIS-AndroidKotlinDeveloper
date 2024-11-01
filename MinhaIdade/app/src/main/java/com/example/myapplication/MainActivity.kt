cf14e93af2ff86b431f38fce43ed978696a9064a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-31 22:10:11 -0300

    finalizacao minha idade, first commit listas-lazy

--- MinhaIdade/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MinhaIdade/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,13 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -11,20 +17,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import com.example.
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            MinhaIdadeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,9 +45,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+@Preview(ShowSystem01 = true)
 @Composable
 fun CounterScreen() {
+    var idade = remember {
+        mutableStateOf(0)
+    }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
@@ -48,28 +59,46 @@ fun CounterScreen() {
         Text(
             text = "Qual é a sua idade?",
             fontSize = 24.sp,
-            color = Color(0xFFAD1F4E)
+            color = Color(0xFFAD1F4E),
+            fontWeight = FontWeight.Bold
         )
         Text(
             text = "Aperte os botões para informar sua idade.",
             fontSize = 12.sp,
         )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
         Text(
-            text = "21",
+            text = "${idade.value}",
             fontSize = 48.sp,
             fontWeight = FontWeight.Bold
         )
+        Spacer(modifier = Modifier.height(32.dp))
+
         Row() {
             Button(
-                onClick = {},
+                onClick = { idade.value-- },
                 modifier = Modifier.size(84.dp),
                 shape = RoundedCornerShape(8.dp),
                 colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
-            ) { Text(
-                text = "-",
-                fontSize = 40.sp
-                ) }
-            Button(onClick = {}) { Text(text = "+") }
+            ) {
+                Text(
+                    text = "-",
+                    fontSize = 40.sp
+                )
+            }
+            Button(
+                onClick = { idade.value++ },
+                modifier = Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
+            ) {
+                Text(
+                    text = "+",
+                    fontSize = 40.sp
+                )
+            }
         }
     }
 }
